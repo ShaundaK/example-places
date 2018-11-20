68cfcc74f29d22be1de1d031e3124455c8648375
ShaundaK <sdking1913@gmail.com>
2018-11-19 22:43:20 -0600

    add places in seed file

--- app/views/api/places/index.json.jbuilder
+++ app/views/api/places/index.json.jbuilder
@@ -1,1 +1,7 @@
-json.message "hello"
+json.array! @places do |places|
+  json.id place.id
+  json.name place.name
+  json.address place.address
+  json.created_at recipe.created_at
+  json.updated_at recipe.updated_at
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create({name: "Walgreens", address: "5748 W. Cook St., Chicago, IL, 60678"})
+Place.create({name: "Target", address: "789 N. Clybourn., Chicago, IL, 60609"})
+Place.create({name: "Walmart", address: "578 W. Desplaines Ave., Chicago, IL, 60699"})
+Place.create({name: "Ikea", address: "999 E. Madision St., Chicago, IL, 60666"})
+Place.create({name: "Menards", address: "888 W. Division St., Chicago, IL, 60655"})
+Place.create({name: "Sams Club", address: "646 E. Augusta St., Chicago, IL, 60678"})
